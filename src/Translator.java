0fa975a73c462c8ea1382552551de9c95d81f444
Hugo Manrique-Pinell <hmanriqu@mail.umw.edu>
2023-09-09 20:48:17 -0400

    Finished

--- src/Translator.java
+++ src/Translator.java
@@ -1,11 +1,18 @@
 
 class Translator {
+               
+                
 
         public static void main(String args[]){
-
-
+                Sentence sentence = new Sentence();
+                String english = sentence.engPrompt();
+                String cleanStr = sentence.cleanString(english);
+                String[] strArr = sentence.strToArr(cleanStr);
+                boolean[] yesUse = sentence.yesIgPay(strArr);
+                String[] translatedArr = sentence.translate(strArr , yesUse);
+                String translatedSentence = sentence.translatedOutput(translatedArr);
+                System.out.println("Pig: " + translatedSentence);               
         }
-
-
+        
 
 }
